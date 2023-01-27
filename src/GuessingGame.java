c98493ff3b1e28765321991aa61c27720335f902
Zeky84 <97308027+Zeky84@users.noreply.github.com>
2023-01-26 20:41:43 -0500

    trevor assignment2

---
+++ src/GuessingGame.java
@@ -0,0 +1,15 @@
+
+public class GuessingGame {
+    public static void main(String[] args) {
+        //the range of my guessing
+        int minNumber =1;int maxNumber = 101;
+        //guessing opportunities
+        int guessingOpportunities = 4;
+        //creating my object
+        GuessingGameData number = new GuessingGameData();
+        //creating my number to guess
+        int numberToGuess = number.generateRandomNumber(minNumber,maxNumber);
+        //guessing the number
+        number.guessingNumber(minNumber,maxNumber,number,guessingOpportunities,numberToGuess);
+    }
+}
---
+++ src/GuessingGameData.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGameData {
+    int generateRandomNumber(int minNumber, int maxNumber){
+        //Generates the random number
+        Random randomNumber = new Random();
+        return randomNumber.nextInt(minNumber,maxNumber);
+    }
+    int giveMeYourNumber(){
+        //Input the user number
+        Scanner numberInput = new Scanner(System.in);
+        String strNumber = numberInput.next();
+        return Integer.parseInt(strNumber);
+    }
+    void guessingNumber(int minNumber,int maxNumber, GuessingGameData number,
+                        int guessingOpportunities, int numberToGuess) {
+        //Guessing the number
+        System.out.println("Pick a number between " + minNumber + " and " + maxNumber);
+
+        while (true) {
+            //user number
+            int userChoiceNumber = number.giveMeYourNumber();
+            if (minNumber > userChoiceNumber || userChoiceNumber > maxNumber) {
+                System.out.print("Your guess is not between " + minNumber + " and " + maxNumber + " .Please try again.");
+            }
+            else {
+                if (guessingOpportunities == 0) {
+                    System.out.println("You lose!, the number to guess was: " + numberToGuess);
+                    break;
+                } else if (userChoiceNumber == numberToGuess) {
+                    System.out.print("You Win!!!");
+                    break;
+                } else if (userChoiceNumber < numberToGuess) {
+                    System.out.print("Please pick a higher number");
+                    guessingOpportunities--;
+                } else if (userChoiceNumber > numberToGuess) {
+                    System.out.print("Please pick a lower number.");
+                    guessingOpportunities--;
+                }
+            }
+        }
+    }
+}
