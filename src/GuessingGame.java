006e54b03b4b79231a1319e378425d57e3604623
Zeky84 <97308027+Zeky84@users.noreply.github.com>
2023-01-29 16:28:58 -0500

    Update GuessingGame.java

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -2,7 +2,7 @@
 public class GuessingGame {
     public static void main(String[] args) {
         //the range of my guessing
-        int minNumber =1;int maxNumber = 101;
+        int minNumber =1;int maxNumber = 100;
         //guessing opportunities
         int guessingOpportunities = 4;
         //creating my object
